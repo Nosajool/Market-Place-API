e3d0b270ebf0a64d57db8ef72e496bb7ae7f1318
Nosajool <jmloo@uwaterloo.ca>
2015-05-17 03:02:27 -0400

    product counter, custom validator

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Market Place API
 
 Learning some API best practices from [APIs On Rails](http://apionrails.icalialabs.com/book) :smile:
+
+## Stuff Covered
+
+- RESTful JSON API
+- API Versioning
+- API Constraints
+- Devise Authentication
+- Active Model Serialization
+- Action Mailer
+- Custom Validators
+- Pagniation
+- Delayed Jobs
+- API Caching
+- Lots and Lots of Tests
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,22 @@ class Order < ActiveRecord::Base
   has_many :products, :through => :placements
 
   validates :user_id, :presence => true
+  validates_with EnoughProductsValidator
 
   before_validation :set_total!
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = 0
+    placements.each do |placement|
+      self.total += placement.product.price * placement.quantity
+    end
+  end
+
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      id, quantity = product_id_and_quantity
+
+      self.placements.build(product_id: id)
+    end
   end
 end
